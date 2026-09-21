d3b376ee88cc32510a05a571d934b4c92e38bf74
IMProRail <arnoud.deboer@prorail.nl>
2026-09-21 12:55:19 +0200

    Update config.mjs

--- js/config.mjs
+++ js/config.mjs
@@ -8,7 +8,7 @@ loadRespecWithConfiguration({
   specType: "HR",
   pubDomain: "dk",
   shortName: "template",
-  publishDate: "2023-06-21",
+ // publishDate: "2023-06-21",
   publishVersion: "0.0.3",
 
   // TODO: Verwijder voordat de release plaats vindt
